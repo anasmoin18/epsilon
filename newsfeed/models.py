88aed60827ea3cf98fc916ff31f344bd6ef14f5b
anasmoin18 <anas.moin173@gmail.com>
2017-04-25 09:37:06 +0530

    code refactored

--- newsfeed/models.py
+++ newsfeed/models.py
@@ -4,10 +4,3 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
-
-class MyUser(models.Model):
-    user_name = models.OneToOneField(User)
-    email = models.EmailField(default='email', max_length=255, unique=True, blank=False)
-
-    def __unicode__(self):
-        return u'{}'.format(self.email)
